6601be03f50cb3b1343492069050d1bb7ef9efce
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-06-25 23:31:20 +0700

    add

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 // require('dotenv').load()
-const port = 3000
+const port = 3031
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
