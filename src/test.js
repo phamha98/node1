5a36bb9608209bee8a091a2366d962609931ad1d
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-07-01 08:28:12 +0700

    add

--- src/test.js
+++ src/test.js
@@ -106,17 +106,17 @@
 // app.listen(port)
 
 // console.log('RESTful API server started on: ' + port)
-const express = require('express')
-const app = express()
-const port = 8081
+// const express = require('express')
+// const app = express()
+// const port = 8081
 
-app.get('/', function (req, res) {
-  res.send('Hello World NODE JS 3031')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World NODE JS 3031')
+// })
 
-app.listen(port, function (error) {
-  if (error) {
-    console.log('Something went wrong')
-  }
-  console.log('server is running port:  ' + port)
-})
+// app.listen(port, function (error) {
+//   if (error) {
+//     console.log('Something went wrong')
+//   }
+//   console.log('server is running port:  ' + port)
+// })
