5f0c482320ccdb80a94c6bafe45416e3b04d9214
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-08-01 21:07:58 +0700

    add

--- src/server.js
+++ src/server.js
@@ -1,108 +1,10 @@
-// const express = require('express')
 import express from 'express'
-// import { routes } from './routes.js' //importing route
 const bodyParser = require('body-parser')
-// import 'dotenv/config'
-// import { Server } from 'socket.io'
-// import http from 'http'
-// import { Log } from './Log.js'
-// import multer from 'multer'
-// import formidable from 'formidable'
-// import siofu from 'socketio-file-upload'
-// import { RouterApp } from './RouterApp.js'
-// import moment from 'moment'
-const process = require('process');
 const app = express()
 const port = 8081
-// const route = new RouterApp()
-// const app = route.app
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(siofu.router)
-// routes(route)
-// const server = http.createServer(app)
-
-/**
- *
- *
- */
-// let memory = {
-//   register: {}
-// }
-// const io = new Server(server)
-
-// // Xử lý các sự kiện của socket
-// io.on('connection', socket => {
-//   Log.g('on-connection', socket.id)
-//   Log.g('register', memory.register)
-//   var uploader = new siofu()
-//   uploader.dir = 'uploads'
-//   uploader.listen(socket)
-//   socket.on('register', e => {
-//     if (e && typeof e == 'object') {
-//       const { user_id } = e
-//       memory.register[user_id] = { user_id, time: new Date() }
-//       Log.d('memory ', memory)
-//       io.emit('register', {
-//         data: e,
-//         msg: 'dang ky thanh cong',
-//         status: true
-//       })
-//     }
-//   })
-
-//   socket.on('upload', event => {
-//     Log.e('upload', event)
-//     var uploader = new siofu()
-//     uploader.dir = 'uploads'
-//     uploader.listen(socket)
-//   })
-//   socket.on('getAll', e => {
-//     io.emit('getAll', io.sockets.sockets)
-//   })
-//   socket.on('disconnect', () => {
-//     Log.e('on-disconnect', socket.id)
-//     // delete memory.clients[socket.id]
-//   })
-// })
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads')
-//   },
-//   filename: function (req, file, cb) {
-//     Log.e('file: ', file)
-//     cb(null, `img${new Date().getTime()}_` + file.originalname)
-//   }
-// })
-
-// var upload1 = multer({ storage: storage })
-
-// app.post('/file', upload1.array('file'), function (req, res, next) {
-//   const files = req.files;
-//   const fileLinks = [];
-//   console.log('file', files)
-//   // Construct link paths for each uploaded file
-//   const prefix = 'http://' + req.hostname + ":" + process.env.SEVER_PORT
-//   files.forEach((file) => {
-//     const linkPath = prefix + `/uploads/${file.filename}`;
-//     fileLinks.push(linkPath);
-//   });
-
-//   return res.json({ files: fileLinks, status: true, });
-// })
-
-/**
- *
- */
-
-// server.listen(3001, () => {
-//   console.log('http:3001')
-// })
-// app.use('/uploads', express.static('uploads'));
-
 app.get('/', (req, res) => {
-  // Display a list of links to the files in the "uploads" directory
-  // You can dynamically generate this list based on the files present in the "uploads" directory
   res.send(`
   <!DOCTYPE html>
 <html lang="en">
@@ -114,7 +16,7 @@ app.get('/', (req, res) => {
         body {
             margin: 0;
             padding: 0;
-            background-image: url("https://img.freepik.com/free-vector/realistic-casino-gambling-illustration_52683-99917.jpg");
+            background-image: url("https://elmaliturta.com/wp-content/uploads/2021/09/hinh-nen-gai-xinh-cho-macbook-1024x576.jpg");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
