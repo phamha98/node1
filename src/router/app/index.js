fd5785717cb7ab1ea6622d6f567381b7dc065ec2
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-08-08 23:57:13 +0700

    111

--- src/router/app/index.js
+++
@@ -1,8 +0,0 @@
-// import Auth from "../Auth/auth";
-
-// const httpRouterApp = new HttpRouter("/app");
-// httpRouterApp.link("/auth", Auth);
-
-// httpRouterApp.list.forEach((value) => console.log(value))
-
-// export default httpRouterApp;
--- src/server1.js
+++ src/server1.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>testestetsets</title>
+    <title>11111</title>
     <style>
         body {
             margin: 0;
