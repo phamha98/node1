9aecb27996cae03fcd326dd7eded02d55b6aa625
Phạm Trần Quang Hà <phamtranquangha@gmail.com>
2023-08-08 10:12:53 +0700

    add

--- src/server1.js
+++ src/server1.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Full Background HTML Template</title>
+    <title>123123123123</title>
     <style>
         body {
             margin: 0;
